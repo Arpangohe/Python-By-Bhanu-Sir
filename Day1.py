01e77632656bc1647c4489092e842ddf45ecd2c4
arpangohe <arpangohe02@gmail.com>
2024-09-27 15:32:17 +0530

    Day 1 Concept

--- Day1.py
+++ Day1.py
@@ -6,3 +6,14 @@ a=int(input("Enter Your Password"))
 print("welcome")
 print("to")
 print("python")
+
+# standard output -> print()
+# standard input -> input()
+# n numbers of parameters -> ,
+ # what is function -> a block of code that performs a specific task.
+ #-> function is used to avoid repetition of code.
+ # -> function is used to make the code more modular.
+ # -> function is used to make the code more readable.
+ # -> function is used to make the code more maintainable.
+
+print("welcome","to","python")
