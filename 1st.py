0af20df708c7e6bddc7a82619e05a6ca523b2db4
arpangohe <arpangohe02@gmail.com>
2024-10-14 14:52:25 +0530

    Day 1

---
+++ 1st.py
@@ -0,0 +1,16 @@
+name = "Arpan"
+age = 22
+location = "Bhopal"
+
+st_detail = [name,age,location]
+st_detail_2 = ["Ajay", 34, "Ujjan"]
+
+print(type(name))
+print(type(age))
+print(type(location))
+print(type(st_detail))
+print(st_detail[0],st_detail[-2])
+
+print(st_detail_2)
+st_detail_2[-2]= "Indore"
+print(st_detail_2)
