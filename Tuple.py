bccb1f7998cae15708f914097f3c5209b57c9020
arpangohe <arpangohe02@gmail.com>
2024-10-16 14:45:55 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+fruit = ("apple", 20, 100)
+
+print(type(fruit))
+print(id(fruit))
+print(id(fruit[0]), id(fruit[1]), id(fruit[2]))
+
+fruit = ("apple",20,100) + ("banana",999)
+print(fruit)
+    # Note : 
