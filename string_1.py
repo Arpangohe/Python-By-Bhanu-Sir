874528110c3639ee747a8b84ef8df79978efba54
arpangohe <arpangohe02@gmail.com>
2024-10-17 15:22:50 +0530

    String Function

---
+++ string_1.py
@@ -0,0 +1,14 @@
+msg = "apple is good 4 health"
+
+# capitlize() --> return Capitalized String
+a=msg.capitalize()
+print(a)
+
+# Upper function 
+#upper()
+b=msg.upper()
+print(b)
+
+#Lower()
+c=msg.lower()
+print(c)
