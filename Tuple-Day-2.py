12e30aeed33208997d283d87f710c618da08ab76
arpangohe <arpangohe02@gmail.com>
2024-10-17 15:19:29 +0530

    Tuple

---
+++ Tuple-Day-2.py
@@ -0,0 +1,23 @@
+number = (12,99) + (78,90)
+
+number = number + (31,12)
+
+print(number)
+           
+# Original tuple with a list as its third element
+t = (34, 44, [3, 4, 55])
+
+# Replace the second element of the list with 99
+t[2][1] = 99
+
+# Print the modified tuple
+print(t)
+
+lis = [["ajay","aman"],(45,[55,23])]
+
+lis [1][1][0]=9999
+
+print(lis)
+
+a = lis.count(1)
+print(a)
